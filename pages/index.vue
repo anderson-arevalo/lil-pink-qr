6752317e67f8b4c7268fe0b3426fd93f4f023700
anderson-arevalo <anderson.arevalo@fastmoda.com.co>
2023-12-09 21:08:06 -0500

    working autentification but with out handle error form server middleware, api and client call

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,41 @@
 <script setup>
+const document = ref('');
+const token = ref('');
 
-const { data, pending, error, refresh } = await useFetch(`/api/auth?document=1020792346&token=C355fd`)
+async function login() { 
+
+  const { data: response, pending, refresh, execute, error} = await useAsyncData('login', () => {
+      try {
+        return $fetch(`/api/auth?document=${document.value}&token=${token.value}`);
+      } catch (error) {
+        console.log('error');
+      }
+    });
+
+    if(toRaw(response.value.data.status)){    
+
+      localStorage.setItem("user_name", response.value.data.user_name);
+      localStorage.setItem("user_last_name", response.value.data.user_last_name);
+      localStorage.setItem("user_io", response.value.data.user_io);
+      localStorage.setItem("token", response.value.data.token);
+      localStorage.setItem("status", response.value.data.status);
+      localStorage.setItem("session_io", response.value.data.session_io);
+    }
+}
 
 </script>
 
 <template>
-<div>{{data}}</div>
   Inicio de sesion
+  <div class="flex gap-4">
+    <input type="text" v-model="document" class="rounded border-dashed text-pink-500" />
+    <input type="password" v-model=token class="rounded  border-dashed text-pink-500" />
+    <button
+        @click="login()"
+        class="inline-block border-2 border-slate-900 border-dashed rounded-lg bg-white hover:bg-slate-100 border px-5 py-3 text-center text-sm font-semibold text-slate-900 sm:w-auto"
+        href=""
+      >
+        Ingresar
+      </button>
+  </div>
 </template>
